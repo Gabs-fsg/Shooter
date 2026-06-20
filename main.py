47f18410ae96e1516cfc72ed21e51296c72a298d
Gabs-fsg <gabinandagarcia@gmail.com>
2026-06-20 19:36:51 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+print('Iniciando setup...')
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+print('Finalizando setup...')
+
+print('Iniciando loop...')
+while True:
+    # Checagem de todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Fecha janela
+            quit() # Finaliza pygame
+
+
